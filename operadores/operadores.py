bd98f34df07797af9fc9c6c2e711f162f42c1c92
César Cláudio Cattermol <cc.catter@gmail.com>
2023-08-08 09:08:52 -0300

    Operadores Aritméticos

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,6 +1,7 @@
 a = 3
 b = -4
 
+#Operadores aritméticos
 soma = a + b
 sub = a - b
 mult = a * b
@@ -14,3 +15,25 @@ print(f"Divisão: { div }")
 print(f"Divisão de inteiros: { div_inteiros }")
 print(f"Cálculos dentro da divisão: { a * b }")
 print(f"Resto da divisão de 16 por a: { 16 % a }")
+
+# Oporadores Relacionais
+print(f"a == b: {a==b}")
+print(f"a < 5: {a < 5}")
+print(f"a > b: {a > b}")
+print(f"a <= 3: {a <= 3}")
+print(f"a >= 4: {a >= 4}")
+print(f"a != b: {a != b}")
+
+c = a != b
+print(f"c: {c}")
+print(f"tipo da var c: {type(c)}") # Bool é o tipo booleano ou lógico
+
+# Operadores Lógicos
+logic1 = True
+logic2 = False
+print(f"type(logic1): {type(logic1)}")
+print(f"type(logic2): {type(logic2)}")
+
+print(f"Logic1: {logic1}")
+print(f"not logic1: {not logic1}")
+print(f"logic1 and logic2: {logic1 and logic2}")
